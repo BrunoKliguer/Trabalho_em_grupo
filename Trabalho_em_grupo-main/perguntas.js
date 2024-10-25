b2ce7aa35b547fc5407db0096475e2ec06b3d77b
BrunoKliguer <144816952+BrunoKliguer@users.noreply.github.com>
2024-10-25 08:18:52 -0300

    Add files via upload

---
+++ Trabalho_em_grupo-main/perguntas.js
@@ -0,0 +1,17 @@
+criaCartao(
+    'Progamação',
+    "O que é Python?",
+    "O Python é uma linguagem de rpogramação"
+)
+
+criaCartao(
+    'Geografia',
+    'Qual a capiral da França?',
+    'A captial da França é Paris'
+)
+
+criaCartao(
+    'Programação',
+    'O que é função?',
+    'Uma função é um bloco de código que executa alguma tarefa'
+)
