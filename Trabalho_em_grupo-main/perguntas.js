5caefed047a6fe17614caec04ae3239be4c6db24
BrunoKliguer <144816952+BrunoKliguer@users.noreply.github.com>
2024-10-30 11:40:24 -0300

    Add files via upload

--- Trabalho_em_grupo-main/perguntas.js
+++ Trabalho_em_grupo-main/perguntas.js
@@ -1,17 +1,23 @@
 criaCartao(
-    'Progamação',
-    "O que é Python?",
-    "O Python é uma linguagem de rpogramação"
+    'Programação',
+    'O que é Python?',
+    'O Python é uma linguagem de programação'
 )
 
 criaCartao(
     'Geografia',
-    'Qual a capiral da França?',
-    'A captial da França é Paris'
+    'Qual a capital da França?',
+    'A capital da França é Paris'
 )
 
 criaCartao(
     'Programação',
-    'O que é função?',
+    'O que é uma função?',
     'Uma função é um bloco de código que executa alguma tarefa'
+)
+
+criaCartao(
+    'Lingua inglesa',
+    'Como se diz oi em Inglês?',
+    'Oi em ingles é HI (RAI)'
 )
